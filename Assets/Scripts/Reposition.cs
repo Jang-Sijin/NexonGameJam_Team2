ad92bd48bf387b08f9eb11aa5fa3c7c5f2b075c2
milk--way <joming1418@gmail.com>
2023-07-22 03:58:58 +0900

    tilemap

--- Assets/Scripts/Reposition.cs
+++ Assets/Scripts/Reposition.cs
@@ -20,23 +20,39 @@ public class Reposition : MonoBehaviour
         // 플레이어 위치
         Vector3 playerPos = Managers.instance._player.transform.position;
         // 타입맵 위치
-        Vector3 myPos = transform.position; 
+        Vector3 myPos = transform.position;
 
         // 플레이어와 타이맵 거리 절대값
-        float difX = Mathf.Abs(playerPos.x - myPos.x);
-        float difY = Mathf.Abs(playerPos.y - myPos.y);
+        float dirX = playerPos.x - myPos.x;
+        float dirY = playerPos.y - myPos.y;
 
         // 이동 방향
-        Vector3 playerDir = Managers.instance._player._inputVector;
-        float dirX = playerDir.x < 0 ? -1 : 1;
-        float dirY = playerDir.y < 0 ? -1 : 1;
+        float difX = Mathf.Abs(dirX);
+        float difY = Mathf.Abs(dirY);
+
+        dirX = dirX > 0 ? 1 : -1;
+        dirY = dirY > 0 ? 1 : -1;
+
 
         switch (transform.tag)
         {
             // 타일맵 이동
             case "Ground":
-                if (difX > difY) transform.Translate(Vector3.right * dirX * _tileMoveDistance);
-                else if (difX < difY) transform.Translate(Vector3.up * dirY * _tileMoveDistance); 
+                if (Mathf.Abs(difX - difY) <= 3f)
+                {
+                    transform.Translate(Vector3.up * dirY * _tileMoveDistance);
+                    transform.Translate(Vector3.right * dirX * _tileMoveDistance);
+                }
+                else if (difX >= difY)
+                {
+                    transform.Translate(Vector3.right * dirX * _tileMoveDistance);
+                    Debug.Log("rigt");
+                }
+                else if (difX <= difY)
+                {
+                    transform.Translate(Vector3.up * dirY * _tileMoveDistance);
+                    Debug.Log("up");
+                }
                 break;
 
             //case "Enemy":
