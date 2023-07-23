7f062eeba1ce422dd843d07dec46f9ea529aa8c7
milk--way <joming1418@gmail.com>
2023-07-23 11:45:32 +0900

    경험치 수정

--- Assets/Scripts/Exprience.cs
+++ Assets/Scripts/Exprience.cs
@@ -6,7 +6,7 @@ public class Exprience : MonoBehaviour
 {
     public int exp;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
